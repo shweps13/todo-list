f8cc6b61dc5164f434d5a004dda8fedefd3bdf05
Joe Siburov <g.siburov@gmail.com>
2025-07-10 13:27:51 -0700

    basic setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -22,8 +23,16 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: {
+      react: { version: 'detect' }
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'warn',
+      'react/prop-types': 'off',
     },
   },
 ])
