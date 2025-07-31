a845a3d79b2cc091905d00e162a769ff71725d5b
Joe Siburov <g.siburov@gmail.com>
2025-07-31 13:53:05 -0700

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,6 @@ import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 
 function App() {
-  // const todos = [
-  //   { id: 1, title: "review resources" },
-  //   { id: 2, title: "take notes" },
-  //   { id: 3, title: "code out app" },
-  // ]
-
   const [todoList, setTodoList] = useState([]);
 
   const addToDo = (title) => {
