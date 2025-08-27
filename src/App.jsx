b0ed74646a399ea50088bd5e733b2b86b5958e04
Joe Siburov <g.siburov@gmail.com>
2025-08-26 18:49:24 -0700

    DB connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,55 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import TodoList from './features/TodoList/TodoList'
 import TodoForm from './features/TodoForm'
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetchTodos();
+  }, [])
+
+  const fetchTodos = async () => {
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+    const token = `Bearer ${import.meta.env.VITE_PAT}`;
+    const options = {
+      // "method": "GET",
+      "Content-Type": "application/json",
+      "Authorization": token
+    }
+
+    try {
+      const response = await fetch(url, {
+        headers: options,
+      });
+
+      if (!response.ok) {
+        setErrorMessage(`Response status: ${response.status}`)
+        return
+      }
+
+      const result = await response.json();
+
+      const fetchedToDos = result.records.map((record) => {
+        const restructedToDo = { id: record.id }
+        restructedToDo.isCompleted = record.fields.isCompleted ?? false;
+        restructedToDo.title = record.fields.title ?? "";
+
+        return restructedToDo
+      })
+
+      setTodoList(fetchedToDos)
+    } catch (error) {
+      setErrorMessage(error.status)
+    } finally {
+      setIsLoading(false)
+    }
+
+  }
 
   const addToDo = (title) => {
     const newTodo = { id: Date.now(), title, isCompleted: false }
@@ -16,6 +61,7 @@ function App() {
       if (todo.id === selectedToDo.id) {
         return { ...todo, isCompleted: true }
       } else {
+        console.log(todo)
         return todo
       }
     })
@@ -39,10 +85,16 @@ function App() {
     <div>
       <h1>Todo List</h1>
       <TodoForm addToDo={addToDo} />
-      {filteredTodoList.length === 0 ?
-        <p>Add todo above to get started</p> :
-        <TodoList todos={filteredTodoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo}/>
-      }
+      <TodoList todos={filteredTodoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo} isLoading={isLoading} />
+      {errorMessage != "" ?
+        <div>
+          <hr />
+          <p>Error happened</p>
+          <p>{errorMessage}</p>
+          <button onClick={() => { setErrorMessage("") }}>Dismiss</button>
+          <hr />
+        </div>
+        : <></>}
     </div>
   )
 }
--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,21 +1,27 @@
 import { useState } from 'react';
 import TodoListItem from './TodoListItem'
 
-function TodoList({ todos, onCompleteTodo, onUpdateTodo }) {
+function TodoList({ todos, onCompleteTodo, onUpdateTodo, isLoading }) {
     const [currEditingId, setCurrEditingId] = useState(null);
 
     return (
-        <ul>
-            {todos.map(todo => {
-                return <TodoListItem
-                    key={todo.id}
-                    todo={todo}
-                    onCompleteTodo={() => onCompleteTodo(todo)}
-                    onUpdateTodo={onUpdateTodo}
-                    currEditingId={currEditingId}
-                    setCurrEditingId={setCurrEditingId} />;
-            })}
-        </ul>
+        <div>
+            {
+                isLoading ?  <p>ToDo list is loading...</p> :
+
+                    <ul>
+                        {todos.map(todo => {
+                            return <TodoListItem
+                                key={todo.id}
+                                todo={todo}
+                                onCompleteTodo={() => onCompleteTodo(todo)}
+                                onUpdateTodo={onUpdateTodo}
+                                currEditingId={currEditingId}
+                                setCurrEditingId={setCurrEditingId} />;
+                        })}
+                    </ul>
+            }
+        </div>
     );
 }
 
