00d7c7a2f793deea6333e0491df1ff89642987fa
Joe Siburov <g.siburov@gmail.com>
2025-09-17 18:28:20 -0700

    reducer created

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -29,42 +29,75 @@ function reducer(state = initialState, action) {
     switch (action.type) {
         case actions.fetchTodos:
             return {
+                isLoading: true,
                 ...state
             }
         case actions.loadTodos:
+            const fetchedToDos = action.records.map((record) => ({
+                id: record.id,
+                isCompleted: record.fields.isCompleted ?? false,
+                title: record.fields.title ?? "",
+            }));
             return {
+                todoList: fetchedToDos,
+                isLoading: false,
                 ...state,
             };
         case actions.setLoadError:
             return {
+                errorMessage: action.error.message,
+                isLoading: false,
                 ...state,
             };
+
         case actions.startRequest:
             return {
+                isSaving: true,
                 ...state,
             };
         case actions.addTodo:
+            const savedTodo = {
+                id: action.records[0].id,
+                title: action.records[0].fields?.title ?? "",
+                isCompleted: !!action.records[0].fields?.isCompleted,
+            };
             return {
+                todoList: [...state.todoList, savedTodo],
+                isSaving: false,
                 ...state,
             };
         case actions.endRequest:
             return {
+                isSaving: false,
                 ...state,
             };
-        case actions.updateTodo:
-            return {
-                ...state,
-            };
-        case actions.completeTodo:
-            return {
-                ...state,
-            };
+
         case actions.revertTodo:
+        case actions.updateTodo:
+            const updatedTodos = state.todoList.map((todo) => 
+                todo.id === action.editedTodo.id ? { ...todo, title: action.editedTodo.title } : todo
+            );
+            const updatedState = {
+                todoList: updatedTodos,
+                isSaving: false,
+                ...state,
+            };
+            if (action.error) {
+                updatedState.errorMessage = action.error.message;
+            }
+            return updatedState;
+        case actions.completeTodo:
+            const completedTodos = state.todoList.map((todo) => 
+                todo.id === action.completedTodo.id ? { ...todo, isCompleted: true } : todo
+            );
             return {
+                todoList: completedTodos,
+                isSaving: false,
                 ...state,
             };
         case actions.clearError:
             return {
+                errorMessage: "",
                 ...state,
             };
     }
